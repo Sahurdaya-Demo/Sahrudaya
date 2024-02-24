d06b8f52e0699729dbcfc5485552956fb16c762b
Sayoo123 <sayoojsunil2000@gmail.com>
2024-02-24 16:15:15 +0530

    commit

--- backend/project/src/components/counsellor/Counsellor.jsx
+++ backend/project/src/components/counsellor/Counsellor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link, Outlet, useNavigate,useLocation } from 'react-router-dom';
 import { useEffect,useState} from 'react';
 import { Button, Spinner,Image,Form} from 'react-bootstrap';
