85661f041f46a6d4c6b795dcee5c0909299379cd
Sayoo123 <sayoojsunil2000@gmail.com>
2024-03-03 22:40:51 +0530

    charts

--- backend/project/src/components/admin/Admindash.jsx
+++ backend/project/src/components/admin/Admindash.jsx
@@ -6,20 +6,34 @@ import { Bar, Doughnut, Line } from "react-chartjs-2";
 function Admindash()
 {
     const[data,setdata]=useState([])
+    const [males, setmales] = useState([]);
+    const [females, setfemales] = useState([]);
+    const [others, setothers] = useState([]);
+    // var datas;
     useEffect(()=>{
         LoadExternalScript(['https://code.jquery.com/jquery-3.7.0.js','https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js','https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js','https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js','https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js','https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js','https://cdn.datatables.net/responsive/2.1.0/js/dataTables.responsive.min.js','https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js','adminjs/tablescript.js']);
         new PureCounter();
         retrieve()
       },[])
       const retrieve=async()=>{
+        
         const response= await fetch(`http://127.0.0.1:8000/formsubmit/`)
         const jsonData = await response.json();
         setdata(jsonData)
         // console.log(new Date(jsonData[1].date).getMonth()==2)
-        let pendingRecords=jsonData.filter(record=>new Date(record.date).getMonth()==2).length
-        console.log(pendingRecords)
+       
+        for(let i=0;i<12;i++){
+            males.push(jsonData.filter(record=>record.gender==='Male'&&new Date(record.date).getMonth()===i).length)
+            females.push(jsonData.filter(record=>record.gender==='Female'&&new Date(record.date).getMonth()===i).length)
+            others.push(jsonData.filter(record=>record.gender==='Others'&&new Date(record.date).getMonth()===i).length)
+        // console.log(pendingRecords)
+        
+        }
+        // male=males[2]
+        // console.log(males[2],females[0],others[0])
+        
       }
-
+    //   console.log(artists[0])
       defaults.maintainAspectRatio=false;
       defaults.responsive=true;
     return(
@@ -143,23 +157,32 @@ function Admindash()
                                         Area Chart Example
                                     </div>
                                     <div className="card-body" style={{position:'relative'}}>
+                                         
                                         <Bar
                                             data={{
                                                 labels:["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEPT","OCT","NOV","DEC"],
                                                 datasets:[
                                                     {
                                                         label:"MALES",
-                                                        data:[200,300,500],
+                                                        data:[males[0],males[1],males[2],males[3],males[4],males[5],males[6],males[7],males[8],males[9],males[10],males[11]],
+                                                        // data:[ar]
                                                     },
                                                     {
                                                         label:"FEMALES",
-                                                        data:[90,80,70],
+                                                        data:[females[0],females[1],females[2],females[3],females[4],females[5],females[6],females[7],females[8],females[9],females[10],females[11]],
                                                     },
                                                     {
                                                         label:"OTHERS",
-                                                        data:[200,300,500],
+                                                        data:[others[0],others[1],others[2],others[3],others[4],others[5],others[6],others[7],others[8],others[9],others[10],others[11]],
                                                     },
                                                 ],
+                                                // datasets:[
+                                                //     {
+                                                //         label:"Males",
+                                                //         data:[100,200,300]
+                                                //     }
+
+                                                // ],
                                             }}
                                             style={{height:'400px'}}
                                         />
