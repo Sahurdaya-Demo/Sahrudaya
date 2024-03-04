1b09051afcc743358d1f0590ad7e592d2dde774f
Sayoo123 <sayoojsunil2000@gmail.com>
2024-03-04 11:09:16 +0530

    done

--- backend/project/src/components/admin/Admindash.jsx
+++ backend/project/src/components/admin/Admindash.jsx
@@ -324,30 +324,6 @@ const options = {
                                     </div>
                                     </div>
                                     
-                            {/* <div className="col-xl-6">
-                                <div className="card mb-4"> */}
-                                    {/* <div className="card-header">
-                                        <i className="fa fa-chart-bar me-1"></i>
-                                        Bar Chart Example
-                                    </div> */}
-                                    {/* <div className="card-body"> */}
-                                    {/* <Carousel autoPlay={false} infiniteLoop={true} showThumbs={false} showArrows={true}  showIndicators={false} showStatus={false} className=' h-100 w-100'>
-                                        <div className='flex flex-row'>
-                                            <div className='w-50'>
-                                            <img src='../../../assets/team/team-3.JPG'style={{maxHeight:'305px'}}/>
-                                            </div>
-                                            <div className=' w-50' style={{backgroundColor:'blue'}}>b</div>
-                                        </div>
-                                        <div className='flex flex-row'>
-                                            <div className='w-50'>
-                                            <img src='../../../assets/team/team-2.JPG' style={{maxHeight:'305px'}}/>
-                                            </div>
-                                            <div className=' w-50' style={{backgroundColor:'blue'}}>b</div>
-                                        </div>
-                                    </Carousel> */}
-                                    {/* </div> */}
-                                {/* </div>
-                            </div>*/}
                         </div> 
                         <div className="card mb-4">
                             <div className="card-header">
