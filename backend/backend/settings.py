836d4d699acaf2b4510a0c2937ff1a02698e21eb
Sayoo123 <sayoojsunil2000@gmail.com>
2024-03-14 15:30:36 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -184,5 +184,6 @@ from django.core.servers.basehttp import WSGIServer
 WSGIServer.handle_error = lambda *args, **kwargs: None
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
